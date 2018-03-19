42029ed41fbc3b75df12ea1c131cee010056b210
Rachel Miles <milesrachel9@gmail.com>
2018-03-19 10:40:35 -0600

    more styling

--- src/App.js
+++ src/App.js
@@ -8,25 +8,45 @@ import {
 } from 'semantic-ui-react';
 import styled from 'styled-components';
 
-const AppContainer=styled.div`
+const AppContainer = styled.div`
   background: linear-gradient(to bottom right, aliceblue, black);
 `
 
+const Transparent = styled.div`
+  background: transparent !important;
+`
+
+const HeaderText = styled.h1`
+  color: white !important;
+  text-align: center;
+  font-size: ${ props => fontSize(props.fSize)} !important;
+`
+
+const fontSize = (size) => {
+  switch (size) {
+    case 'large':
+      return '4rem';
+    case 'small':
+      return '1rem';
+    default:
+      return '2rem';
+  }
+}
+
 const App = () => (
   <AppContainer>
-    <Header as="h1" textAlign="center">
+    <Header fSize='large' as={HeaderText}>
       My Portfolio
     </Header>
-    <Segment>
-      <Header as='h2' textAlign='center'>
+    <Segment as={Transparent}>
+      <Header as={HeaderText}>
         My Projects
       </Header>
     </Segment>
-      <Header as='h2' textAlign='center'>
+    <Segment as={Transparent}>
+      <Header as={HeaderText}>
         Contact
       </Header>
-    <Segment>
-
     </Segment>
   </AppContainer>
 )
